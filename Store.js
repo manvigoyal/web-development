42d54ac6cae4493592b75c1d8588f6f287d05529
Manvi Goyal <79033458+manvigoyal@users.noreply.github.com>
2021-07-01 10:22:11 +0530

    Add files via upload

---
+++ Store.js
@@ -0,0 +1,9 @@
+function Store(){
+    return(
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+          <img src="manvi" alt=""/>
+            
+        </div>
+    );
+}
+export default Store;
